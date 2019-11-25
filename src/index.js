c9800e8f8dbefe0e51a803dc6539b1af6d13a58f
samyabouseda <samy@abouseda.ch>
2019-11-25 17:31:01 +0100

    Remove storybook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
-import './index.css';
+import './index.css'
 import store from './store'
 import Root from './components/Root'
 
 ReactDOM.render(
-    <Root store={store} />,
-    document.getElementById('root')
-);
+	<Root store={store} />,
+	document.getElementById('root'),
+)
+
+if (module.hot) {
+	module.hot.accept()
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()

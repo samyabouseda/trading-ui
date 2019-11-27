47a993cf4c89408a8a672f68100faef9f982a795
samyabouseda <samy@abouseda.ch>
2019-11-27 16:38:47 +0100

    Add e2e tests for trader signup feature

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -15,45 +15,25 @@ class LoginScreen extends Component {
 	render() {
 		const { isSignup } = this.state
 		return (
-			<div className={styles['login-screen']}>
+			<div
+				className={styles['login-screen']}
+				data-testid="login-screen"
+			>
 				<Card>
-					<div>
-						<p className={styles.title}>
-							{isSignup ? 'SIGNUP' : 'LOGIN'}
-						</p>
-						<p className={styles.guidelines}>
-							Fill in the form to{' '}
-							{isSignup ? 'create' : 'access'} your
-							account.
-						</p>
-						<p
-							className={styles.link}
-							onClick={this.toggleSignupLoginView}
-						>
-							{isSignup
-								? 'Already registered?'
-								: "Don't have an account yet?"}
-						</p>
-						<form>
-							<Input
-								name="username"
-								label="username"
-								type="text"
-								placeholder="John"
-							/>
-							<Input
-								name="password"
-								label="password"
-								type="password"
-								placeholder="Password"
-							/>
-							<Button color="primary">
-								{isSignup
-									? 'Create account'
-									: 'Login'}
-							</Button>
-						</form>
-					</div>
+					{isSignup && (
+						<SignupForm
+							toggleSignupLoginView={
+								this.toggleSignupLoginView
+							}
+						/>
+					)}
+					{!isSignup && (
+						<LoginForm
+							toggleSignupLoginView={
+								this.toggleSignupLoginView
+							}
+						/>
+					)}
 				</Card>
 			</div>
 		)
@@ -65,5 +45,59 @@ class LoginScreen extends Component {
 		}))
 	}
 }
+
+const SignupForm = ({ toggleSignupLoginView }) => (
+	<div>
+		<p className={styles.title}>SIGNUP</p>
+		<p className={styles.guidelines}>
+			Fill in the form to create your account.
+		</p>
+		<p className={styles.link} onClick={toggleSignupLoginView}>
+			Already registered?
+		</p>
+		<form data-testid="signup-form">
+			<Input
+				name="username"
+				label="username"
+				type="text"
+				placeholder="John"
+			/>
+			<Input
+				name="password"
+				label="password"
+				type="password"
+				placeholder="Password"
+			/>
+			<Button color="primary">Create account</Button>
+		</form>
+	</div>
+)
+
+const LoginForm = ({ toggleSignupLoginView }) => (
+	<div>
+		<p className={styles.title}>LOGIN</p>
+		<p className={styles.guidelines}>
+			Fill in the form to access your account.
+		</p>
+		<p className={styles.link} onClick={toggleSignupLoginView}>
+			Don't have an account yet?
+		</p>
+		<form data-testid="login-form">
+			<Input
+				name="username"
+				label="username"
+				type="text"
+				placeholder="John"
+			/>
+			<Input
+				name="password"
+				label="password"
+				type="password"
+				placeholder="Password"
+			/>
+			<Button color="primary">Login</Button>
+		</form>
+	</div>
+)
 
 export default LoginScreen
---
+++ src/e2e/trader-signup.test.js
@@ -0,0 +1,39 @@
+/**
+ Scénario : Un nouveau trader s’enregistre sur la plateforme
+ 	ETANT DONEE que le trader se trouve sur la page de création de compte
+ 	LORSQUE le trader remplis les champs « username » et « password » avec ses informations
+ 	ET click sur le bouton « Create account »
+ 	ALORS il doit voir un message de confirmation avec son username et sa clé privée
+*/
+
+const sel = testid => `[data-testid="${testid}"]`
+const ROUTES = {
+	SIGNUP: `http://localhost:3000/`,
+}
+
+describe('Scénario : Un nouveau trader s’enregistre sur la plateforme', async () => {
+	it('ETANT DONEE que le trader se trouve sur la page de création de compte', async () => {
+		await page.goto(ROUTES.SIGNUP)
+		await page.waitForSelector(sel('login-screen'))
+	})
+
+	it('LORSQUE le trader remplis les champs « username » et « password » avec ses informations', async () => {
+		const values = {
+			username: 'James',
+			password: 'Bond',
+		}
+		await expect(page).toFillForm(sel('signup-form'), values)
+	})
+
+	it('ET click sur le bouton "Create account"', async () => {
+		await expect(page).toClick('button', {
+			text: 'Create account',
+		})
+	})
+
+	it('ALORS il doit voir un message de confirmation avec son username et sa clé privée', async () => {
+		await page.waitForSelector(sel('signup-success-message'))
+		await page.waitForSelector(sel('username-field'))
+		await page.waitForSelector(sel('private-key-field'))
+	})
+})

f3110ff76190465101903fb012b75e4f188181b8
samyabouseda <samy@abouseda.ch>
2019-11-28 16:10:40 +0100

    Add styles to error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Dialog from '../Dialog'
 import Button from '../Button'
+import warningIcon from './warning-icon.png'
+import styles from './Error.module.css'
 
 const Error = ({ message, buttonText, dismiss }) => (
 	<Dialog>
 		<div>
-			<p>Warning</p>
-			<p>Something went wrong</p>
-			<p>{message}</p>
+			<img
+				className={styles.icon}
+				src={warningIcon}
+				alt="error icon"
+			/>
+			<p className={styles.title}>Something went wrong.</p>
+			<p className={styles.message}>{message}</p>
 			<Button color="primary" onClick={dismiss}>
 				{'Dismiss'}
 			</Button>

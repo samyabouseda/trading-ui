eb6fcf4fb512bf5f1db3ee72872695a25b9aeb5b
samyabouseda <samy@abouseda.ch>
2019-11-30 14:11:12 +0100

    Refactor Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,21 +1,26 @@
 import React from 'react'
 
 const Form = ({ onSubmit, children }) => {
-	//TODO: refactor handleSubmit
 	const handleSubmit = event => {
 		event.preventDefault()
 		const elements = Object.values(event.target.elements)
 		const inputs = elements.filter(e => e.localName === 'input')
-		const arr = inputs.map(input => ({
+		const inputsKeyValuePairs = getKeyValuePairsFrom(inputs)
+		const data = constructDataObjectWith(inputsKeyValuePairs)
+		onSubmit(data)
+	}
+
+	const getKeyValuePairsFrom = inputs =>
+		inputs.map(input => ({
 			name: input.name,
 			value: input.value,
 		}))
-		const data = arr.reduce((map, obj) => {
+
+	const constructDataObjectWith = inputsKeyValuePairs =>
+		inputsKeyValuePairs.reduce((map, obj) => {
 			map[obj.name] = obj.value
 			return map
 		}, {})
-		onSubmit(data)
-	}
 
 	return (
 		<div>
